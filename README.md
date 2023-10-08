43255a52f788a83eb6dde8f8312622700c15ed23
ManabBala <manabbalaofficial@gmail.com>
2023-10-09 00:43:09 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Discord Webcam Auto Mirror Disabler
+
+## Issue:-
+
+Discord always mirror the webcam feed of client in the room/channel preview section as like someone is looking into a mirror. This is normal to many other app like zoom, google meet etc.
+
+But If someone's webcam already mirroring the feed itself or for use of "OBS Virtual Webcam" this behaviour is not needed/annoying. There is no way of discord knowing if the feed is a virtual webcam or not.
+
+## Solution:-
+
+There is no option to toggle this functionality in "Discord". So I made this extension which will delete the "mirror" class form the video element thus showing the actual feed from the "Virtual Cam".
---
+++ content.js
@@ -0,0 +1,38 @@
+function injectCode() {
+	console.log("Discord Webcam Auto Mirror Disabler Launched");
+	// Function to handle the appearance of new video elements
+	function handleNewVideoElements(mutationsList) {
+		mutationsList.forEach((mutation) => {
+			mutation.addedNodes.forEach((node) => {
+				if (node instanceof HTMLElement) {
+					// trying to find video element with the mirror class
+					let cls = "mirror-";
+					let element = node.querySelector(`video[class*="${cls}"]`);
+
+					// if video element found then delete the class
+					if (element) {
+						console.log("Mirror Class Deleted From Webcam Video Element.");
+						element.classList.remove.apply(
+							element.classList,
+							Array.from(element.classList).filter((v) => v.startsWith(cls))
+						);
+					}
+				}
+			});
+		});
+	}
+
+	// Create a Mutation Observer
+	const observer = new MutationObserver(handleNewVideoElements);
+
+	// Define the configuration for the observer
+	const config = {
+		childList: true, // Watch for changes in the child nodes of the target
+		subtree: true, // Watch for changes in the entire subtree
+	};
+
+	// Start observing the DOM
+	observer.observe(document.body, config);
+}
+
+injectCode(); // Call the function to execute the code immediately
